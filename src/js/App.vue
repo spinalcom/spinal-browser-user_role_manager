e23a2f2950696da36270a6ebcf5d38ddf268be42
laurent <htd.laurent@gmail.com>
2019-07-26 18:56:25 +0200

    fix default edtion mode

--- src/js/App.vue
+++ src/js/App.vue
@@ -41,8 +41,7 @@ export default {
     return {
       users: [],
       userProfiles: [],
-      // editionDisable: true,
-      editionDisable: false,
+      editionDisable: true,
       showUserDialog: false,
     };
   },
